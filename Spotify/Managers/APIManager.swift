6b6460e8a73fce5f19ec1e4e24166be70a026fa7
Rami Elwan <ramielwan48@gmail.com>
2024-07-30 15:03:46 +0200

    chore: commit changes

--- Spotify/Managers/APIManager.swift
+++ Spotify/Managers/APIManager.swift
@@ -9,76 +9,75 @@ import Foundation
 
 final class APIManager {
     static let shared = APIManager()
-    
+
     private init() {}
-    
-    struct Constants {
+
+    enum Constants {
         static let baseUrl = "https://api.spotify.com/v1"
     }
-    
+
     enum APIError: Error {
         case UnAuthenticated
         case FailedToGetData
     }
-    
-    
+
     public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
         guard let accessToken = AuthManager.shared.accessToken else {
             completion(.failure(APIError.UnAuthenticated))
             return
         }
-        
+
         var request = URLRequest(url: URL(string: "\(Constants.baseUrl)/me")!)
         request.httpMethod = HttpMethod.GET.rawValue
         request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
             guard let data, error == nil else {
                 completion(.failure(APIError.FailedToGetData))
                 return
             }
-            
+
             do {
                 let jsonDecoder = JSONDecoder()
                 jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-                
-                completion(.success(try jsonDecoder.decode(UserProfile.self, from: data)))
+
+                try completion(.success(jsonDecoder.decode(UserProfile.self, from: data)))
             } catch {
                 print(error)
                 completion(.failure(error))
             }
         }
-        
+
         task.resume()
     }
-    
-    public func getCurrentUserProfile(completion: @escaping (Result<UserProfile, Error>) -> Void) {
+
+    public func getNewReleases(completion: @escaping (Result<NewReleasesResponse, Error>) -> Void) {
         guard let accessToken = AuthManager.shared.accessToken else {
             completion(.failure(APIError.UnAuthenticated))
             return
         }
-        
-        var request = URLRequest(url: URL(string: "\(Constants.baseUrl)/me")!)
+
+        var request = URLRequest(url: URL(string: "\(Constants.baseUrl)/browse/new-releases")!)
         request.httpMethod = HttpMethod.GET.rawValue
         request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
             guard let data, error == nil else {
                 completion(.failure(APIError.FailedToGetData))
                 return
             }
-            
+
             do {
                 let jsonDecoder = JSONDecoder()
                 jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-                
-                completion(.success(try jsonDecoder.decode(UserProfile.self, from: data)))
+
+                try completion(.success(jsonDecoder.decode(NewReleasesResponse.self, from: data)))
             } catch {
                 print(error)
                 completion(.failure(error))
             }
         }
-        
+
         task.resume()
     }
 }
---
+++ Spotify/Models/Image.swift
@@ -0,0 +1,14 @@
+//
+//  Image.swift
+//  Spotify
+//
+//  Created by Rami Elwan on 30.07.24.
+//
+
+import Foundation
+
+struct Image: Codable {
+    let url: String
+    let height: Int
+    let width: Int
+}
