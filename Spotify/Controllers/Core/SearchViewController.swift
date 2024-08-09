d5670d4f399cdd932463e6581ee1979b488cfb70
Rami Elwan <ramielwan48@gmail.com>
2024-08-09 19:40:21 +0200

    feat: display mock genres

--- Spotify/Controllers/Core/SearchViewController.swift
+++ Spotify/Controllers/Core/SearchViewController.swift
@@ -8,12 +8,96 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    let searchController: UISearchController = {
+        let vc = UISearchController(searchResultsController: SearchResultsViewController())
+        vc.searchBar.placeholder = "Songs, Artists, Albums"
+        vc.searchBar.searchBarStyle = .default
+        vc.definesPresentationContext = true
+        
+        return vc
+    }()
+    
+    private let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: UICollectionViewCompositionalLayout(
+            sectionProvider: { _, _ in
+                
+                let countPerRow = 2
+                
+                let item = NSCollectionLayoutItem(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1/CGFloat(countPerRow)),
+                        heightDimension: .uniformAcrossSiblings(estimate: 60)
+                    )
+                )
+                
+                let group = NSCollectionLayoutGroup.horizontal(
+                    layoutSize: NSCollectionLayoutSize(
+                        widthDimension: .fractionalWidth(1),
+                        heightDimension: .estimated(60)
+                    ),
+                    subitems: Array(repeating: item, count: countPerRow)
+                )
+                
+                group.interItemSpacing = .fixed(12)
+                
+                let section = NSCollectionLayoutSection(group: group)
+                section.interGroupSpacing = 24
+                
+                return section
+            }
+        )
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        searchController.searchResultsUpdater = self
         view.backgroundColor = .systemBackground
+        navigationItem.searchController = searchController
+        setupCollectionView()
     }
     
-
 }
+
+extension SearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    private func setupCollectionView(){
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(GenreCollectionViewCell.self, forCellWithReuseIdentifier: GenreCollectionViewCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.frame = view.bounds
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GenreCollectionViewCell.identifier, for: indexPath) as? GenreCollectionViewCell else {
+            fatalError()
+        }
+        cell.configure(with: "Rock")
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchResultsController = searchController.searchResultsController as? SearchResultsViewController,
+              let query = searchController.searchBar.text?.trimmingCharacters(in: .whitespaces),
+              query.count > 0 else {
+            return
+        }
+        
+        print(query)
+    }
+    
+    
+}
+
---
+++ Spotify/View/GenreCollectionViewCell.swift
@@ -0,0 +1,99 @@
+//
+//  GenreCollectionViewCell.swift
+//  Spotify
+//
+//  Created by Rami Elwan on 09.08.24.
+//
+
+import UIKit
+
+class GenreCollectionViewCell: UICollectionViewCell {
+    static let identifier = "GenreCollectionViewCell"
+    private static let IMAGE_SIZE: CGFloat = 60
+    
+    private let colors: [UIColor] = [
+        .systemRed,
+        .systemGreen,
+        .systemBlue,
+        .systemPink,
+        .systemPurple,
+        .systemOrange,
+        .systemTeal,
+        .systemMint,
+        .systemCyan,
+        .systemBrown,
+        .systemIndigo,
+    ]
+    
+    private let imageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.tintColor = .white
+        imageView.image = UIImage(
+            systemName: "music.quarternote.3",
+            withConfiguration: UIImage.SymbolConfiguration(pointSize: 50)
+        )
+        
+        return imageView
+    }()
+    
+    private let label = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.layer.cornerRadius = 8
+        contentView.layer.masksToBounds = true
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func setupConstraints() {
+        let mainStack = UIStackView()
+        mainStack.translatesAutoresizingMaskIntoConstraints = false
+        mainStack.isLayoutMarginsRelativeArrangement = true
+        mainStack.layoutMargins = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        mainStack.axis = .vertical
+        
+        let topStackView = UIStackView()
+        topStackView.addArrangedSubview(imageView)
+        topStackView.axis = .vertical
+        topStackView.alignment = .trailing
+        
+        let bottomStackView = UIStackView()
+        bottomStackView.addArrangedSubview(label)
+        bottomStackView.axis = .vertical
+        bottomStackView.alignment = .leading
+
+        mainStack.addArrangedSubview(topStackView)
+        mainStack.addArrangedSubview(bottomStackView)
+        mainStack.distribution = .fill
+        mainStack.spacing = 8
+        
+        contentView.addSubview(mainStack)
+        
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: GenreCollectionViewCell.IMAGE_SIZE),
+            imageView.heightAnchor.constraint(equalToConstant: GenreCollectionViewCell.IMAGE_SIZE),
+
+            mainStack.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            mainStack.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            mainStack.topAnchor.constraint(equalTo: contentView.topAnchor),
+            mainStack.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ])
+    }
+    
+    func configure(with title: String){
+        label.text = title
+        contentView.backgroundColor = colors.randomElement()
+    }
+}
